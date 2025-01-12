41261e8725373de84beb86485522e70acc9804f6
danielpolak2000 <daniel.polak@web.de>
2025-01-12 18:36:48 +0100

    import now going into kafka

--- src/data-collector/data_collector.py
+++ src/data-collector/data_collector.py
@@ -1,8 +1,7 @@
 from kafka import KafkaProducer
-import time
 import json
 import os
-import random
+import pandas as pd
 
 # Kafka configuration
 KAFKA_BROKER = os.environ.get("KAFKA_BROKER", "kafka-cluster-kafka-bootstrap.kafka:9092")
@@ -24,17 +23,26 @@ def main():
         value_serializer=lambda v: json.dumps(v).encode("utf-8")
     )
 
+    csv_file = "bitcoin-historical-data.csv"
+    data = pd.read_csv(csv_file)
+
+    # Konvertiere Timestamps
+    data['Timestamp'] = pd.to_datetime(
+        data['Timestamp'], unit='s').dt.tz_localize("UTC")
+    data.set_index("Timestamp", inplace=True)
+
     # Continuously fetch and publish real-time data
-    while True:
-        price = 40000 + random.uniform(-500, 500)  # Add random variation to the price
+    for index, row in data.iterrows():
         fetched_data = {
-            "name": "Bitcoin",
-            "symbol": "BTC",
-            "timestamp": time.time(),
-            "price": price
+            "timestamp": data['Timestamp'],
+            "coin": "BTC",
+            "open": row["Open"],
+            "high": row["High"],
+            "low": row["Low"],
+            "close": row["Close"],
+            "volume": row["Volume"]
         }
         publish_to_kafka(producer, KAFKA_TOPIC, fetched_data)
-        time.sleep(10)  # Wait before fetching data again
 
 if __name__ == "__main__":
     main()
